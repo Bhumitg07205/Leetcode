bc504cca120e27d9b99d7f1b6ed999eaf0bdaee7
Bhumit Gupta <142826072+Bhumitg07205@users.noreply.github.com>
2026-04-28 18:31:14 +0530

    Time: 26 ms (78.6%), Space: 87 MB (78.6%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> nums;
+        int target_rem = grid[0][0] % x;
+        
+        for (const auto& row : grid) {
+            for (int val : row) {
+                if (val % x != target_rem) return -1;
+                nums.push_back(val);
+            }
+        }
+        sort(nums.begin(), nums.end());
+        int median = nums[nums.size() / 2];
+        int ops = 0;
+        
+        for (int val : nums) {
+            ops += abs(val - median) / x;
+        }
+        
+        return ops;
+    }
+};
