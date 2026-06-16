8ff008ef16553e4b9f98c03bba83fb955c76f2fb
Bhumit Gupta <142826072+Bhumitg07205@users.noreply.github.com>
2026-06-16 22:22:16 +0530

    Time: 2 ms (79.92%), Space: 39.7 MB (69.7%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result;
+        for(char c:s){
+            switch(c){
+                case '*': if (!result.empty())result.pop_back();break;
+                case '#': result+=result;break;
+                case '%': reverse(result.begin(),result.end());break;
+                default: result+=c;break;
+            }
+        }
+        return result;
+    }
+};
