e8e3ad08aa0af47569de655e9a556bc3ee7b315b
Bhumit Gupta <142826072+Bhumitg07205@users.noreply.github.com>
2026-06-19 15:55:41 +0530

    Time: 3 ms (23.8%), Space: 23.7 MB (72%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,20 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int hieght(TreeNode* root) {
+//         if (root == NULL)
+//             return 0;
+
+//         return 1 + max(hieght(root->left), hieght(root->right));
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         if(root==NULL)return 0;
+//         int D1=hieght(root->left)+hieght(root->right);
+//         int D2= diameterOfBinaryTree(root->left);
+//         int D3=diameterOfBinaryTree(root->right);
+//         return max(D1,max(D2,D3));
+
+//     }
+// };
 class Solution {
 public:
-    int hieght(TreeNode* root) {
-        if (root == NULL)
-            return 0;
+    struct HDPair {
+        int height;
+        int diameter;
+    };
 
-        return 1 + max(hieght(root->left), hieght(root->right));
+    HDPair optDiameter(TreeNode* root) {
+        HDPair p;
+
+        if (root == NULL) {
+            p.height = 0;
+            p.diameter = 0;
+            return p;
+        }
+
+        HDPair Left = optDiameter(root->left);
+        HDPair Right = optDiameter(root->right);
+
+        p.height = max(Left.height, Right.height) + 1;
+
+        int D1 = Left.height + Right.height;
+        int D2 = Left.diameter;
+        int D3 = Right.diameter;
+
+        p.diameter = max(D1, max(D2, D3));
+
+        return p;
     }
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(root==NULL)return 0;
-        int D1=hieght(root->left)+hieght(root->right);
-        int D2= diameterOfBinaryTree(root->left);
-        int D3=diameterOfBinaryTree(root->right);
-        return max(D1,max(D2,D3));
 
+    int diameterOfBinaryTree(TreeNode* root) {
+        return optDiameter(root).diameter;
     }
 };
