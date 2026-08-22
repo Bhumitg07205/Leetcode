a94a2cab0ea22493181d7a74c2748573e2043a8d
Bhumit Gupta <142826072+Bhumitg07205@users.noreply.github.com>
2026-08-22 22:34:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (37.42%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int temp = n;
+        int digit_sum = 0;
+        int digit_product = 1;
+        while (temp > 0) {
+            int digit = temp % 10;
+            digit_sum += digit;
+            digit_product *= digit;
+            temp /= 10;
+        }
+        
+        int combined_total = digit_sum + digit_product;
+        return n % combined_total == 0;
+    }
+};
